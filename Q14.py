12a0c093491b5c94170736ecf5cdadb087cef8b8
Heth1643 <95498709+Heth1643@users.noreply.github.com>
2021-12-04 19:50:22 +0530

    Update Q14.py

--- Q14.py
+++ Q14.py
@@ -1,2 +1,5 @@
-# Help-together
-Learn something
+p = int(input("Enter Principal Amount: "))
+r = int(input("Enter  ratevof interests: "))
+n = int(input("Enter number of years"))
+i = (p * r * n) / 100
+print(i)
