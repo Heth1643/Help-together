faba0f5b29728bf13442e46f3c0883d39bf42863
Heth1643 <95498709+Heth1643@users.noreply.github.com>
2021-12-04 19:46:08 +0530

    Update Q8.py

--- Q8.py
+++ Q8.py
@@ -1,2 +1,6 @@
-# Help-together
-Learn something
+length = int(input("Enter length"))
+
+perimeter = length * 4
+area = length * length
+print("Perimenter of Square is", perimeter)
+print("Area of Square is", area)
