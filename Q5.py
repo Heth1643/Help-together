76a48a84b0c615052dd5e1210138dacd076ded65
Heth1643 <95498709+Heth1643@users.noreply.github.com>
2021-12-04 19:43:37 +0530

    Update Q5.py

--- Q5.py
+++ Q5.py
@@ -1,2 +1,7 @@
-# Help-together
-Learn something
+num1 = int(input("enter number 1: "))
+num2 = int(input("enter number 2: "))
+
+print("Addition ",num1 + num2)
+print("Subtraction ",num1 - num2)
+print("multiflication ",num1 * num2)
+print("Division ",num1 / num2)
