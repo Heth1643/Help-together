98ddb3de71ea92b40aa8f0b04c67b11489daaeac
Heth1643 <95498709+Heth1643@users.noreply.github.com>
2021-12-04 19:48:30 +0530

    Update Q11.py

--- Q11.py
+++ Q11.py
@@ -1,2 +1,11 @@
-# Help-together
-Learn something
+#Celcius:
+celcius = int(input("Enter Celcius to convert into fahrenhit: "))
+
+f = (celcius * 9/5) + 32
+
+print(f)
+
+fah = int(input("Enter fahrenhit to convert into celcius: "))
+
+c = (fah - 32) * 5/9
+print(c)
